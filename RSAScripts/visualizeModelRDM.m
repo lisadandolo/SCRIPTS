b53e1d502c09f1752f60dd2508ffd5ae888ba23e
Lisa Catherine Dandolo <Lisa Catherine Dandolo>
2017-04-21 18:04:38 +0200

    changed a few model RDMs -not very successful

--- RSAScripts/visualizeModelRDM.m
+++ RSAScripts/visualizeModelRDM.m
@@ -9,9 +9,8 @@ userOptions.rootPath = 'C:\Users\penv395\Desktop\transMem\fMRI_study\Results\fMR
 
 % Prepare the model RDMs.
 RDMs_model = constructModelRDMs(makeModelRDMs_Pictures, userOptions);
-RDMs_model2 = constructModelRDMs(makeModelRDMs_PicturesIndividualLevel,userOptions);
+
 
 % Visualize the modelRDMs
 figureRDMs(RDMs_model, userOptions, struct('fileName', 'modelRDMs', 'figureNumber', 3));
 
-figureRDMs(RDMs_model2, userOptions, struct('fileName', 'modelRDMs', 'figureNumber', 3));
