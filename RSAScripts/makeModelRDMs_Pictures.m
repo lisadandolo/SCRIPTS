4071df0251eabd40e199f30351489832fa2b7861
Lisa Catherine Dandolo <Lisa Catherine Dandolo>
2017-04-24 13:44:52 +0200

    changed model RDMs again

--- RSAScripts/makeModelRDMs_Pictures.m
+++ RSAScripts/makeModelRDMs_Pictures.m
@@ -6,38 +6,6 @@ function Models = makeModelRDMs_Pictures()
 %  with the model's name (use underscores in stead of spaces).
 %  
 %  Cai Wingfield 11-2009
-Models.allConditionsDifferent = kron([
-			0 1 1 
-			1 0 1 
-			1 1 0], ones(60,60));
-
-Models.OldSimAreEqual =  kron([
-			0 0 1 
-			0 0 1 
-			1 1 0], ones(60,60));
-        
-Models.NovSimAreEqual =   kron([
-			0 1 1 
-			1 0 0 
-			1 0 0], ones(60,60));
-        
-Models.OldSimAreMoreComparable =  kron([
-			0    0.5  1 
-			0.5  0    1 
-			1    1    0], ones(60,60));
-        
-Models.NovSimAreMoreComparable =   kron([
-			0    1    1 
-			1    0    0.5 
-			1    0.5  0   ], ones(60,60));
-
-        
-all = ones(180);
-all(1:180+1:end) = 0;
-Models.allPicturesDifferent = all;
-
-Models.allPicturesSame = zeros(180);
-
 
 A = ones(60);
 B = zeros(60);
@@ -48,14 +16,55 @@ D = 0.2 * ones(60);
 D(1:60+1:end) = 0;
 
 E = ones(60);
-E(1:60+1:end) = 0.5;
-   
+E(1:60+1:end) = 0;
+
+F = ones(60);
+F(1:60+1:end) = 0.5;
+
+all = ones(180);
+all(1:180+1:end) = 0;
+
+Models.allPicturesDifferent = all;
+
+Models.allPicturesSame = zeros(180);
+
+Models.allConditionsDifferent = kron([
+			0 1 1 
+			1 0 1 
+			1 1 0], ones(60,60));
+
+Models.OldSimAreEqual =  kron([
+			0 0 1 
+			0 0 1 
+			1 1 0], ones(60,60));
+        
+Models.OldSimAreMoreComparable =  kron([
+			0    0.5  1 
+			0.5  0    1 
+			1    1    0], ones(60,60));   
+        
+        
+Models.OldSimAreMoreComparable2 =   [D, C, A;
+                                     C, D, A;
+                                     A, A, D];
+      
+Models.NovSimAreEqual =   kron([
+			0 1 1 
+			1 0 0 
+			1 0 0], ones(60,60));
+            
+Models.NovSimAreMoreComparable =   kron([
+			0    1    1 
+			1    0    0.5 
+			1    0.5  0   ], ones(60,60));
+
+        
 Models.NovSimAreMoreComparable2 =   [D, A, A;
                                      A, D, C;
                                      A, C, D];
 
-Models.SomeTransformed =   [D, E, A;
-                            E, D, A;
-                            A, A, D];
+Models.OnlyPairsComparable =   [E, F, A;
+                                F, E, A;
+                                A, A, E];
 
 end
